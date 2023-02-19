1820a44a5eb13feeeb530be5b46ef00e59efbd28
DizMaxWork <solskype01@gmail.com>
2023-02-20 00:30:13 +0300

    Начало

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$ (function() {
+    $('.slider').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: true,
+        prevArrow: "<img src='img/prev-arrow.svg' class='prev' alt='1'>",
+        nextArrow: "<img src='img/next-arrow.svg' class='next' alt='2'>",
+      });
+})
